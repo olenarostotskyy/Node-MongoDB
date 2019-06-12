39b6664151508646198ffdff0e151ba83baca36a
Olena Rostotskyy <olenazachkow@gmail.com>
2019-06-12 16:11:55 -0700

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient; //node-module
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';//to start up a connection to the MongoDB server
 const dbname = 'conFusion';//conFusion DB
@@ -11,27 +12,32 @@ MongoClient.connect(url, (err, client) => {// err, client - is a callback wit h2
 
     console.log('Connected correctly to server');
 
+
     const db = client.db(dbname);//to connect to DB
-    const collection = db.collection("dishes");//let us interact with dishes collection
-    collection.insertOne({"name": "Uthappizza", "description": "test"},// inserting one object into the collection
-    (err, result) => {// caLLBACK with 2 params: err and result
-        assert.equal(err,null);
+    
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+    "dishes", (result) => {
+        console.log("Insert Document:\n", result.ops);//OPS tells you the number of insert operations that were carried out
 
-        console.log("After Insert:\n");
-        console.log(result.ops);// 
+        dboper.findDocuments(db, "dishes", (docs) => {
+            console.log("Found Documents:\n", docs);
 
-        collection.find({}).toArray((err, docs) => {//will search for everything that is there in the collection, and then, provide that to us. Then, this can be converted to an array of JSON objects.
-            assert.equal(err,null);
-            
-            console.log("Found:\n");
-            console.log(docs);//all the documents in the collections will be returned
+            dboper.updateDocument(db, { name: "Vadonut" },
+                { description: "Updated Test" }, "dishes",
+                (result) => {
+                    console.log("Updated Document:\n", result.result);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+                    dboper.findDocuments(db, "dishes", (docs) => {
+                        console.log("Found Updated Documents:\n", docs);
+                        
+                        db.dropCollection("dishes", (result) => {
+                            console.log("Dropped Collection: ", result);
 
-                client.close();//will close the cpnnection to the DB
-            });
+                            client.close();
+                        });
+                    });
+                });
         });
-    });
+});
 
 });
